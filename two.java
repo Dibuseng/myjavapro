48ec948a72c9370f0641896929a64bc7d57df0b7
Dibuseng <77392525+Dibuseng@users.noreply.github.com>
2021-01-15 10:50:27 +0200

    Update two.java

--- two.java
+++ two.java
@@ -1,1 +1,28 @@
 Something about java class
+  
+  Ingredients
+2 cups (250g) all-purpose flour (spoon & leveled)
+1 teaspoon baking soda
+1/4 teaspoon salt
+1/2 teaspoon ground cinnamon
+1/2 cup (1 stick or 115g) unsalted butter, softened to room temperature
+3/4 cup (150g) packed light or dark brown sugar
+2 large eggs, at room temperature
+1/3 cup (80g) plain yogurt or sour cream (I use Greek yogurt)
+2 cups mashed bananas (about 4 large ripe bananas)
+1 teaspoon pure vanilla extract
+optional: 3/4 cup (100g) chopped pecans or walnuts
+Instructions
+Adjust the oven rack to the lower third position and preheat the oven to 350°F (177°C). Grease a 9×5-inch loaf pan or coat with nonstick spray. Set aside.
+Whisk the flour, baking soda, salt, and cinnamon together in a large bowl.
+Using a handheld or stand mixer fitted with a paddle or whisk attachment, beat the butter and brown sugar together on high speed until smooth and creamy, about 2 minutes. On medium speed, add the eggs one at a time, beating well after each addition. Beat in the yogurt, mashed bananas, and vanilla extract on medium speed until combined. With the mixer running on low speed, slowly beat the dry ingredients into the wet ingredients until no flour pockets remain. Do not overmix. Fold in the nuts, if using.
+Spoon the batter into the prepared baking pan and bake for 60-65 minutes. Loosely cover the bread with aluminum foil after 30 minutes to help prevent the top and sides from getting too brown. A toothpick inserted in the center of the loaf will come out clean when the bread is done. Remove from the oven and allow the bread to cool completely in the pan set on a wire rack.
+Cover and store banana bread at room temperature for 2 days or in the refrigerator for up to 1 week. Banana bread tastes best on day 2 after the flavors have settled together. See post above for freezing instructions.
+Notes
+Butter: If needed, you can use salted butter in this recipe instead. Keep all the other ingredients the same.
+Cream Cheese Frosting: This banana bread also tastes fantastic with cream cheese frosting on top! To make it, beat 4 ounces (112g) of softened cream cheese and 1/4 cup (60g) of softened unsalted butter together on medium speed until smooth. Beat in 1 cup (120g) of confectioners sugar, 1/2 teaspoon of pure vanilla extract, and a pinch of salt until combined. Spread on cooled loaf.
+Banana Bread Muffins: Use this banana bread recipe to make 2 dozen banana bread muffins. Pour the batter into a lined or greased muffin pan and bake for 5 minutes at 425°F (218°C) then, keeping the muffins in the oven, reduce the oven temperature to 350°F (177°C). Bake for an additional 16-17 minutes or until a toothpick inserted in the center comes out clean. The total bake time for the banana bread muffins is about 22-23 minutes. The initial burst of hot air helps those muffins rise nice and tall! Or try my quick & easy banana muffins.
+Brown Sugar: Feel free to substitute the same amount of regular white granulated sugar or coconut sugar. I do not recommend any liquid sweeteners.
+No Sour Cream or Yogurt? Feel free to use 1/3 cup mashed banana (in addition to the 2 cups), unsweetened applesauce, or even canned pumpkin puree.
+Frozen Bananas: You can use frozen bananas. Thaw them at room temperature and drain off any excess liquid before using in the recipe.
+Chocolate Chips: I love this bread with chocolate chips, too. Feel free to add 1 cup of your favorite chocolate chips (semi-sweet, dark chocolate, etc). No need to leave out the nuts if you add the chocolate chips. Chocolate chips and nuts are both optional.
